c3c97023b24ff2514184a07483b1286b2ead0282
DANIEL JAMES <daniel.james@codeimmersives.com>
2020-07-23 10:02:20 -0400

    5 of 10 tests passed

--- main.js
+++ main.js
@@ -1,13 +1,63 @@
-const makeDino = function() {
+/**********************
+ * MAKE DINO FUNCTION *
+ **********************/
+
+const makeDino = function (species, period, carnivore, extinct) {
+    const dino = {}
+    dino.species = species;
+    dino.period = period;
+    dino.carnivore = carnivore;
+    
+    if (extinct === undefined){
+      dino.extinct = false;
+    }
+    else{
+    dino.extinct = extinct;
+    };
+
+    return dino
 }
 
-const makeSingular = function() {
+//---------------------------------------------------------------------------------------//
+//---------------------------------------------------------------------------------------//
+
+
+/**************************
+ * MAKE SINGULAR FUNCTION *
+ **************************/
+//    given a dinosaur object
+//    returns a new dinosaur object
+//    with 'us' ending suffix removed
+const makeSingular = function (dinosaurObject) {
+  //remove last 2 string characters for species' value 
+  //if the last 2 characters are equal to 'us'
+  //.slice()
+  //.splice()
+  let species = dinosaurObject.species
+  // console.log(species)
+  const last2Characters = species.slice(species.length - 2)
+  // console.log(species)
+  let result = species.slice(0, species.length - 2)
+  if (last2Characters === 'us') {
+
+    // console.log(result)
+    return result
+    // }
+    // else {
+    //   return species
+    // }
+    // console.log(last2Characters)
+
+  }
+}
+//---------------------------------------------------------------------------------------//
+//---------------------------------------------------------------------------------------//
+
+
+const truncateSpecies = function () {
 }
 
-const truncateSpecies = function() {
-}
-
-const makeExtinct = function() {
+const makeExtinct = function () {
 
 }
 
